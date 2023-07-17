60a58f3f7f6e57390783c0e812590827b7e09fb6
ferreiraturbo <139795557+ferreiraturbo@users.noreply.github.com>
2023-07-17 14:17:53 -0300

    Mudado a versão para teste
    Versão mudada para teste

--- geraos.user.js
+++ geraos.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gera_OS
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://erp.turbonettelecom.com.br/attendance
