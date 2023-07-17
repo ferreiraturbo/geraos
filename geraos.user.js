32e76ccc724cf05fd8c79864ff2bda0d9e308719
ferreiraturbo <139795557+ferreiraturbo@users.noreply.github.com>
2023-07-17 14:31:48 -0300

    Update geraos.user.js

--- geraos.user.js
+++ geraos.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gera_OS
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://erp.turbonettelecom.com.br/attendance
